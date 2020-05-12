ff3cfedbffeb769da63750d6f73d0e150ba225a8
Etum <catsarebestthing@outlook.com>
2020-05-12 11:48:17 -0400

    Update iptables-count.sh

--- iptables-count.sh
+++ iptables-count.sh
@@ -3,7 +3,7 @@ echo 'Gimme an IP to measure!'
 read ip
 #reset all counters & iptables rules
 iptables -Z && iptables -F
-#measure incoming traffic to x  target(?)
+#measure incoming traffic to ip target
 iptables -I INPUT 1  -s $ip -j ACCEPT
-#measure outgoing traffic to target(?)
+#measure outgoing traffic to ip target
 iptables -I OUTPUT 1 -d $ip -j ACCEPT
